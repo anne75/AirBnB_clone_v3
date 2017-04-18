f318121dfb6438c3a649a5bb486a81629cfd53c6
Richard Sim <rdsim8589@gmail.com>
2017-04-18 15:06:16 -0700

    bring branch up to date. now change in file

--- api/v1/app.py
+++ api/v1/app.py
@@ -16,5 +16,6 @@ def teardown:
 if __name__ == "__main__":
     app = Flask(__name__)
     app.register_blueprint(app_views)
+
     host = getenv(HBNB_API_HOST, "0.0.0.0")
     port = getenv(HBNB_API_PORT, "5000")
