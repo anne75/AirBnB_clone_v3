6cf17dbc1bc8e9a3d868acdf06a30ca65798c7fc
Richard Sim <rdsim8589@gmail.com>
2017-04-18 16:21:42 -0700

    pushing current progress

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,14 +8,16 @@ from models import storage
 from os import getenv
 
 
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
 @app.teardown_appcontext
-def teardown:
+def teardown(exception):
     """ closes the session """
     storage.close()
 
 if __name__ == "__main__":
-    app = Flask(__name__)
-    app.register_blueprint(app_views)
-
-    host = getenv(HBNB_API_HOST, "0.0.0.0")
-    port = getenv(HBNB_API_PORT, "5000")
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
+    print(app.url_map)
+    app.run(host=host, port=port)
