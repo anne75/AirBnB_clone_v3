5c25a9e82aaef0206660d3ba5cd1d5da04273806
Anne <anne.cognet@holbertonschool.com>
2017-04-19 21:44:44 +0000

    finish 7?

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -26,15 +26,38 @@ def delete_state(state_id=None):
     """deletes a state"""
     if state_id is None:
         abort(404)
-    all_states = storage.all("State")
-    state = all_states.get(state_id)
+    state = storage.get("State", state_id)
     if state is None:
         abort(404)
     storage.delete(state)
     return jsonify({})
 
 
-@app_views.route('states', methods=['POST'])
+@app_views.route('/states/', methods=['POST'])
 def create_state():
-    if not request.json:
-        abort(400)
+    try:
+        r = request.get_json()
+        if 'name' not in r.keys():
+            return "Missing name", 400
+        s = State(**r)
+        s.save()
+        return jsonify(s.to_json()), 201
+    except:
+        return "Not a JSON", 400
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def update_state(state_id=None):
+    try:
+        r = request.get_json()
+        state = storage.get("State", state_id)
+        if state is None:
+            abort(404)
+        for k in ("id", "created_at", "updated_at"):
+            r.pop(k, None)
+        for k, v in r.items():
+            setattr(state, k, v)
+        state.save()
+        return jsonify(state.to_json()), 200
+    except:
+        return "Not a JSON", 400
