aff422311acf9355f4d56d81793f52976dae7472
Richard Sim <rdsim8589@gmail.com>
2017-04-19 11:31:36 -0700

    modify to import storage from __init__

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-from api.v1.views import app_views
+from api.v1.views import app_views, storage
 from flask import jsonify
-from models import storage
 """
 Index model holds the endpoint (route)
 """
