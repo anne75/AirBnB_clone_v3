dddc0d0d636882c3ea8cb999f7a8b91163abf2be
Richard Sim <rdsim8589@gmail.com>
2017-04-19 11:27:59 -0700

    added comments

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,10 @@
 #/usr/bin/python3
+"""
+creates the Blueprint for flask application
+"""
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
+#what is the point of this line?
 from api.v1.views.index import *
