87fa64167eba4a108df9e1ee62239f7b9885a7bb
Richard Sim <rdsim8589@gmail.com>
2017-04-19 14:54:13 -0700

    moved comments to after imports

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,11 +1,13 @@
 #/usr/bin/python3
+from flask import Blueprint
+from models import cities
+from models import storage
 """
 creates the Blueprint for flask application
 """
-from flask import Blueprint
-from models import storage
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 #what is the point of this line?
 from api.v1.views.index import *
+from api.v1.views.cities import *
