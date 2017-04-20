07f072964a103879c068eac4ce2dfd9e1494af07
Richard Sim <rdsim8589@gmail.com>
2017-04-20 14:34:11 -0700

    completed the endpoints for reviews and updated __init__.puy to import Review class

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,6 +4,7 @@ from models.amenity import Amenity
 from models.city import City
 from models import storage
 from models.place import Place
+from models.review import Review
 from models.state import State
 from models.user import User
 """
@@ -19,3 +20,4 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.cities import *
 from api.v1.views.places import *
+from api.v1.views.reviews import *
