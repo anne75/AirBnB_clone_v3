2514512ce075bc5d954c7daf054e09d505cbec8d
Richard Sim <rdsim8589@gmail.com>
2017-04-19 21:55:12 -0700

    fixed the PUT and POST request for updating and creating a city

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -3,7 +3,7 @@
 Contains
 """
 from api.v1.views import (app_views, City, storage)
-from flask import (abort, jsonify)
+from flask import (abort, jsonify, request)
 
 
 @app_views.route("/states/<state_id>/cities", methods=["GET"])
@@ -46,28 +46,32 @@ def delete_one_city(city_id):
 @app_views.route("/states/<state_id>/cities", methods=["POST"])
 def create_one_city(state_id):
     try:
+        print(request)
         r = request.get_json()
     except:
         return "Not a JSON", 400
+
     if 'name' not in r.keys():
         return "Missing name", 400
     # creates the dictionary r as kwargs to create a city object
     c = City(**r)
+    c.state_id = state_id
+    c.save()
     return jsonify(c.to_json()), 201
 
 
-@app_views.route("/cities/<city_id>", methods=["PUTS"])
+@app_views.route("/cities/<city_id>", methods=["PUT"])
 def update_one_city(city_id):
     city = storage.get("City", city_id)
     if city is None:
         abort(404)
     try:
-        r= request.get_json()
+        r = request.get_json()
     except:
         return "Not a JSON", 400
     for k in ("id", "created_at", "updated_at", "state_id"):
         r.pop(k, None)
-    for k, v in r.items:
+    for k, v in r.items():
         setattr(city, k, v)
     city.save()
     return jsonify(city.to_json()), 200
