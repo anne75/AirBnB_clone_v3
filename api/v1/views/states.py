c0d4c842f473009d9334a9982ed3c9399adbb2ac
Anne <anne.cognet@holbertonschool.com>
2017-04-25 18:04:31 +0000

    change dict pop

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -58,9 +58,10 @@ def update_state(state_id=None):
     state = storage.get("State", state_id)
     if state is None:
         abort(404)
-    for k in ("id", "created_at", "updated_at"):
-        r.pop(k, None)
+#    for k in ("id", "created_at", "updated_at"):
+#        r.pop(k, None)
     for k, v in r.items():
-        setattr(state, k, v)
+        if k not in ("id", "created_at", "updated_at"):
+            setattr(state, k, v)
     state.save()
     return jsonify(state.to_json()), 200
